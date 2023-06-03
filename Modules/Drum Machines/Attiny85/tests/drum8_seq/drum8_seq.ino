f872d97f88efc8f46b44defad3ad3f0f1c47e428
Ella Jameson <ellagjameson@gmail.com>
2023-06-02 23:51:39 -0700

    Update drum8_seq.ino

--- Modules/Drum Machines/Attiny85/tests/drum8_seq/drum8_seq.ino
+++ Modules/Drum Machines/Attiny85/tests/drum8_seq/drum8_seq.ino
@@ -2,40 +2,71 @@
 
 */
 
-const int trigger_pin = 4;
-const int sample_pin[] = {0, 1, 2};
+const uint8_t trigger_pin = 4;
+const uint8_t sample_pin[] = {0, 1, 2};
 
-int tempo = 120;
-const int trigger_time = 3;
+uint16_t tempo = 120;
+const uint8_t trigger_time = 10;
 
 
-void set_sample(int sample_num) {
-  for(int i = 0; i < 3; i++) {
+const uint8_t drum_pattern[] =
+{
+  0b00000001,
+  0b00000000,
+  0b01000001,
+  0b00000000,
+
+  0b00000010,
+  0b00000000,
+  0b01000000,
+  0b00000010,
+
+  0b00000000,
+  0b00000010,
+  0b01000001,
+  0b00000001,
+
+  0b00000010,
+  0b00000000,
+  0b01000000,
+  0b00000010
+};
+
+
+void set_sample(uint8_t sample_num) {
+  for(uint8_t i=0; i<3; i++) {
     digitalWrite(sample_pin[i], sample_num & (1<<i));
   }
 }
 
-int bpm2ms(int bpm) {
-  return 60000 / bpm;
+uint16_t bpm2ms(uint16_t bpm) {
+  return 15000 / bpm;  // (60,000 / bpm) / 4
 }
 
 
 void setup() {
   pinMode(trigger_pin, OUTPUT);
-  for(int i = 0; i < 3; i++) {
+  for(uint8_t i=0; i<3; i++) {
     pinMode(sample_pin[i], OUTPUT);
   }
 }
 
 void loop() {
-  for(int s = 0; s < 8; s++) {
-    // Set sample
-    set_sample(s);
-    // Trigger
-    digitalWrite(trigger_pin, HIGH);
-    delay(trigger_time);
-    digitalWrite(trigger_pin, LOW);
+  for(uint8_t frame=0; frame<16; frame++) {
+    uint16_t play_delay = 0;
+    for(uint8_t s=0; s<8; s++) {
+      if(drum_pattern[frame] & (1<<s)){
+        // Set sample
+        set_sample(s);
+        // Trigger
+        digitalWrite(trigger_pin, HIGH);
+        delay(trigger_time);
+        digitalWrite(trigger_pin, LOW);
+        delay(trigger_time);
+        play_delay += (trigger_time<<1);
+      }
+    }
     // Wait the rest of the period
-    delay(bpm2ms(tempo)-trigger_time);
+    delay(bpm2ms(tempo)-play_delay);
   }
 }
