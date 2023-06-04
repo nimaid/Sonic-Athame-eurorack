2cf3ce7f3a4d2b02ae3612c73185a100d25ff192
Ella Jameson <ellagjameson@gmail.com>
2023-06-04 04:27:16 -0700

    Update drum8_seq.ino

--- Modules/Drum Machines/Attiny85/tests/drum8_seq/drum8_seq.ino
+++ Modules/Drum Machines/Attiny85/tests/drum8_seq/drum8_seq.ino
@@ -2,17 +2,19 @@
 
 */
 
-const uint8_t trigger_pin = 4;
-const uint8_t shiftreg_data_pin = 2;
-const uint8_t shiftreg_clock_pin = 1;
-const uint8_t shiftreg_latch_pin = 0;
+#define PIN_TRIGGER 4
+#define PIN_SR_D 2
+#define PIN_SR_C 1
+#define PIN_SR_L 0
+
+#define trigger_time 3
+
 
 uint16_t tempo = 120;
-const uint8_t trigger_time = 3;
 
 
-/*
-const uint8_t drum_pattern[] =
+
+uint8_t drum_pattern[] =
 {
   0b00000001,
   0b00000000,
@@ -34,45 +36,23 @@ const uint8_t drum_pattern[] =
   0b01000000,
   0b00000010
 };
-*/
-const uint8_t drum_pattern[] =
-{
-  0b00000001,
-  0b00000000,
-  0b00000000,
-  0b00000000,
 
-  0b00000001,
-  0b00000000,
-  0b00000000,
-  0b00000000,
-
-  0b00000001,
-  0b00000000,
-  0b00000000,
-  0b00000000,
-
-  0b00000001,
-  0b00000000,
-  0b00000001,
-  0b00000000,
-};
 
 
 void shift_out(uint8_t byte_out) {
-  digitalWrite(shiftreg_latch_pin, LOW);
+  digitalWrite(PIN_SR_L, LOW);
   for(uint8_t i=0; i<8; i++) {
     if(byte_out & (0b10000000>>i)) {  // Last bit first
-      digitalWrite(shiftreg_data_pin, HIGH);
+      digitalWrite(PIN_SR_D, HIGH);
     }
     else {
-      digitalWrite(shiftreg_data_pin, LOW);
+      digitalWrite(PIN_SR_D, LOW);
     }
 
-    digitalWrite(shiftreg_clock_pin, HIGH);
-    digitalWrite(shiftreg_clock_pin, LOW);
+    digitalWrite(PIN_SR_C, HIGH);
+    digitalWrite(PIN_SR_C, LOW);
   }
-  digitalWrite(shiftreg_latch_pin, HIGH);
+  digitalWrite(PIN_SR_L, HIGH);
 }
 
 uint16_t bpm2ms(uint16_t bpm) {
@@ -82,31 +62,23 @@ uint16_t bpm2ms(uint16_t bpm) {
 
 
 void setup() {
-  pinMode(trigger_pin, OUTPUT);
-  pinMode(shiftreg_data_pin, OUTPUT);
-  pinMode(shiftreg_clock_pin, OUTPUT);
-  pinMode(shiftreg_latch_pin, OUTPUT);
+  pinMode(PIN_TRIGGER, OUTPUT);
+  pinMode(PIN_SR_D, OUTPUT);
+  pinMode(PIN_SR_C, OUTPUT);
+  pinMode(PIN_SR_L, OUTPUT);
 }
 
 void loop() {
   for(uint8_t frame=0; frame<16; frame++) {
-    uint16_t play_delay = 0;
+    // Set samples to play
+    shift_out(drum_pattern[frame]);
+    // trigger
+    digitalWrite(PIN_TRIGGER, HIGH);
+    delay(trigger_time);
+    digitalWrite(PIN_TRIGGER, LOW);
+    delay(trigger_time);
 
-    shift_out(frame);
-
-    for(uint8_t s=0; s<8; s++) {
-      if(drum_pattern[frame] & (1<<s)){
-        // Set sample
-        //TODO
-        // Trigger
-        digitalWrite(trigger_pin, HIGH);
-        delay(trigger_time);
-        digitalWrite(trigger_pin, LOW);
-        delay(trigger_time);
-        play_delay += trigger_time << 1;
-      }
-    }
     // Wait the rest of the period
-    delay(bpm2ms(tempo) - play_delay);
+    delay(bpm2ms(tempo) - (trigger_time<<1));
   }
 }
